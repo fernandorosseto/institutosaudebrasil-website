b6d84ffaa9314ef6ffb0e330855130b059e27e48
Rosseto <112822062+fernandorosseto@users.noreply.github.com>
2023-09-16 13:14:30 -0300

    atualização do projeto - nova logo, novo design

--- js/script.js
+++ js/script.js
@@ -1,37 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const linkMenus = document.querySelectorAll(".btn");
-  
-    function updateSelectedLink() {
-      const currentHash = window.location.hash;
-      linkMenus.forEach(link => {
-        if (link.getAttribute("href") === currentHash) {
-          link.classList.add("selected");
-        } else {
-          link.classList.remove("selected");
-        }
-      });
-    }
-  
-    // Adiciona a classe "selected" ao link correto quando a página é carregada.
-    updateSelectedLink();
-  
-    // Atualiza a classe "selected" quando houver alterações no hash da URL.
-    window.addEventListener("hashchange", updateSelectedLink);
-});
+const btnHamburger = document.getElementById("btnHamburger");
 
-//Primeira página, rolagem dos slides
-const btnEsquerdo = document.getElementById("btn-esquerdo")
-const btnDireito = document.getElementById("btn-direito")
-
-btnEsquerdo.addEventListener('click', function(){
-  scrollSlides(-2000)
-}) 
-
-btnDireito.addEventListener('click', function(){
-  scrollSlides(2000)
-})
-
-function scrollSlides(valorScroll) {
-  const div = document.getElementById('slides');
-  div.scrollLeft += valorScroll;
+function abrirNav() {
+	const nav = document.getElementById("nav");
+	nav.classList.toggle("active");
 }
+
+btnHamburger.addEventListener("click", abrirNav);
