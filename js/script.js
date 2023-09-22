27f0475baf4da9b16df5b60ad89a8cf2c3288fc8
Rosseto <112822062+fernandorosseto@users.noreply.github.com>
2023-09-22 10:01:27 -0300

    ajustes no css e js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const btnHamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav");
 const menuItens = document.querySelectorAll("#menu a"); // Seleciona todos os links dentro do menu
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 function abrirNav() {
 	nav.classList.toggle("active");
 	btnHamburger.classList.toggle("x");
+	console.log(nav.classList);
 }
-
+console.log(nav.classList);
 btnHamburger.addEventListener("click", abrirNav);
 
 menuItens.forEach(function (item) {
@@ -14,6 +17,12 @@ menuItens.forEach(function (item) {
 		abrirNav(); // Fecha o menu quando um item de menu é clicado
 	});
 });
+
+if (menu.getPropertyValue("visibility") === "visible") {
+	logo.addEventListener("click", function () {
+		abrirNav();
+	});
+}
 
 window.addEventListener("load", function () {
 	window.scrollTo(0, 0);
