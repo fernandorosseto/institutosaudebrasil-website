60f4b24184e7597024ff922646e69f5d834b1d1c
Rosseto <112822062+fernandorosseto@users.noreply.github.com>
2023-09-22 22:53:05 -0300

    ajustando funçôes no Js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 const btnHamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav");
 const menuItens = document.querySelectorAll("#menu a"); // Seleciona todos os links dentro do menu
-const menu = document.getElementById("menu");
 const logo = document.getElementById("logo");
+const menu = document.getElementById("menu");
 
 function abrirNav() {
 	nav.classList.toggle("active");
 	btnHamburger.classList.toggle("x");
-	console.log(nav.classList);
 }
-console.log(nav.classList);
+
 btnHamburger.addEventListener("click", abrirNav);
 
 menuItens.forEach(function (item) {
@@ -18,11 +17,9 @@ menuItens.forEach(function (item) {
 	});
 });
 
-if (menu.getPropertyValue("visibility") === "visible") {
-	logo.addEventListener("click", function () {
-		abrirNav();
-	});
-}
+function fecharNav() {}
+
+logo.addEventListener("click", fecharNav);
 
 window.addEventListener("load", function () {
 	window.scrollTo(0, 0);
